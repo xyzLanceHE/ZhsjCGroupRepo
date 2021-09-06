ae33b6f60b8cce8d7d5e3d3372e91e38e6fde158
shawshank <1524158692@qq.com>
2021-09-06 23:34:29 +0800

    增加地图发布节点

---
+++ Project/MapNode/MapNode.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "TinyROS/TinyROS.h"
+#include <thread>
+
+int flag = 0;
+struct map
+{
+	int localMap[400] = { 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,
+						 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,
+						 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,
+						 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,
+						 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,
+						 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,
+						 0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0 };
+};
+
+void NormalCallback(TinyROS::StringMessage msg)
+{
+	flag = 1;
+}
+int main()
+{
+	map rawMap;
+	TinyROS::SimpleObjectMessage<map> msg(rawMap);
+	//节点初始化
+	try
+	{
+		TinyROS::Node::Init("MapNode");
+	}
+	catch (TinyROS::TinyROSException& e)
+	{
+		std::cout << e.what();
+		return -1;
+	}
+	//定义节点
+	TinyROS::Publisher* mapPublisher;
+	TinyROS::Publisher* mapSubscriber;
+	try
+	{
+		mapPublisher = TinyROS::NewPublisher<TinyROS::SimpleObjectMessage<map>>("localMap");
+	}
+	catch (TinyROS::TinyROSException& e)
+	{
+		std::cout << e.what();
+		return -1;
+	}
+
+	TinyROS::MessageCallback<TinyROS::StringMessage> callback(1);
+	callback.Register(NormalCallback);
+	while (true)
+	{
+		if (flag == 1)
+		{
+			mapPublisher->Publish(msg);
+			break;
+		}
+
+	}
+
+	TinyROS::Node::Close();
+}
