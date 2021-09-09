5ecea8e1df37637a892a5f9457e28454dba0c40f
ht <286085300@qq.com>
2021-09-09 14:20:13 +0800

    从串口获取并处理数据

--- Project/UwbComNode/main.cpp
+++ Project/UwbComNode/main.cpp
@@ -10,175 +10,123 @@
 #include <iostream> 
 #include <fstream> 
 
-int OpenDev(char* Dev)
-{
-    //Dev 就是设备，设备就是文件，就是给出该设备文件的路径
-    int fd = open(Dev, O_RDWR); //| O_NOCTTY | O_NDELAY
-    //printf("%d", fd);
-    if (-1 == fd)
-    {
-        perror("Can't Open Serial Port");
-        return -1;
-    }
-    else
-        return fd;
-}
+#define ComAddress  "/dev/ttyUSB0"
+#define MemoryData 5
 
 /**
-*@brief  设置串口通信速率
-*@param  fd     类型 int  打开串口的文件句柄
-*@param  speed  类型 int  串口速度
-*@return  void
+*@brief  设置串口属性
 */
-int speed_arr[] = {B115200, B38400, B19200, B9600, B4800, B2400, B1200, B300,
-                  B38400, B19200, B9600, B4800, B2400, B1200, B300, };
-int name_arr[] = {115200, 38400,  19200,  9600,  4800,  2400,  1200,  300,
-                  38400, 19200,  9600, 4800, 2400, 1200,  300, };
-void set_speed(int fd, int speed) {
-    int   i;
-    int   status;
-    struct termios   Opt;
-    tcgetattr(fd, &Opt);
-    for (i = 0; i < sizeof(speed_arr) / sizeof(int); i++) {
-        if (speed == name_arr[i]) {
-            /**
-             * tcflush函数刷清(抛弃)输入缓存(终端驱动程序已接收到，但用户程序尚未读)或输出缓存(用户程序已经写，但尚未发送)。queue参数应是下列三个常数之一：
-            * TCIFLUSH刷清输入队列。
-             * TCOFLUSH刷清输出队列。
-            * TCIOFLUSH刷清输入、输出队列。
-             */
-            tcflush(fd, TCIOFLUSH);//设置前flush    
-            cfsetispeed(&Opt, speed_arr[i]);
-            cfsetospeed(&Opt, speed_arr[i]);
-            //通过tcsetattr函数把新的属性设置到串口上。
-            //tcsetattr(串口描述符，立即使用或者其他标示，指向termios的指针)
-            status = tcsetattr(fd, TCSANOW, &Opt);
-            if (status != 0)
-            {
-                perror("tcsetattr fd");
-                return;
-            }
-            tcflush(fd, TCIOFLUSH);  //设置后flush
-        }
-    }
-}
-
-/**
- *@brief   设置串口数据位，停止位和效验位
- *@param  fd     类型  int  打开的串口文件句柄
- *@param  databits 类型  int 数据位   取值 为 7 或者8
- *@param  stopbits 类型  int 停止位   取值为 1 或者2
- *@param  parity  类型  int  效验类型 取值为N,E,O,S
- */
-int set_Parity(int fd, int databits, int stopbits, int parity)
+void setCom(int fd) 
 {
     struct termios options;
     if (tcgetattr(fd, &options) != 0)
     {
-        perror("SetupSerial 1");
-        return (false);
+        perror("SetupSerial");
     }
-    options.c_cflag &= ~CSIZE;
-    switch (databits)
-        /*设置数据位数*/
-    {
-    case 7:
-        options.c_cflag |= CS7;
-        break;
-    case 8:
-        options.c_cflag |= CS8;
-        break;
-    default:
-        fprintf(stderr, "Unsupported data size\n");
-        return (false);
-    }
-    switch (parity)
-    {
-    case 'n':
-    case 'N':
-        options.c_cflag &= ~PARENB; /* Clear parity enable */
-        options.c_iflag &= ~INPCK; /* Enable parity checking */
-        break;
-    case 'o':
-    case 'O':
-        options.c_cflag |= (PARODD | PARENB); /* 设置为奇效验*/
-        options.c_iflag |= INPCK; /* Disnable parity checking */
-        break;
-    case 'e':
-    case 'E':
-        options.c_cflag |= PARENB; /* Enable parity */
-        options.c_cflag &= ~PARODD; /* 转换为偶效验*/
-        options.c_iflag |= INPCK; /* Disnable parity checking */
-        break;
-    case 'S':
-    case 's': /*as no parity*/
-        options.c_cflag &= ~PARENB;
-        options.c_cflag &= ~CSTOPB;
-        break;
-    default:
-        fprintf(stderr, "Unsupported parity\n");
-        return (false);
-    }
-    /* 设置停止位*/
-    switch (stopbits)
-    {
-    case 1:
-        options.c_cflag &= ~CSTOPB;
-        break;
-    case 2:
-        options.c_cflag |= CSTOPB;
-        break;
-    default:
-        fprintf(stderr, "Unsupported stop bits\n");
-        return (false);
-
-    }
-    /* Set input parity option */
-    if (parity != 'n')
-        options.c_iflag |= INPCK;
-    tcflush(fd, TCIFLUSH);
-    options.c_cc[VTIME] = 150; /* 设置超时15 seconds*/
-    options.c_cc[VMIN] = 0; /* Update the options and do it NOW */
+    options.c_cflag |= CLOCAL | CREAD;
+    options.c_cflag &= ~CSIZE;/*设置字符大小*/
+    options.c_cflag |= CS8;
+    options.c_cflag &= ~CSTOPB;//stop bits
+    cfsetispeed(&options, B115200);
+    cfsetospeed(&options, B115200);
+    options.c_cc[VTIME] = 0;
+    options.c_cc[VMIN] = 0;/*设置等待时间和最小接收字符*/
+    tcflush(fd, TCIFLUSH);/*处理未接收字符*/
     if (tcsetattr(fd, TCSANOW, &options) != 0)
     {
-        perror("SetupSerial 3");
-        return (false);
+        perror("com set error!\n");
     }
-    return (true);
+    std::cout << "Configure Completed" << std::endl;
+}
+
+/**
+*@ 将数据从接收的串口信息中提取出来
+* buff：串口信息
+* an：目标信息
+* s：信息位置
+* p：信息存储位置
+*/
+void GetData(char* buff,float* an,char* s,int p)
+{
+    char data[10];
+    if (strstr(buff, s) != NULL)
+    {
+        strncpy(data, strstr(buff, s) + 4, 5);
+        data[5] = '\0';
+        /*printf("%s", data);
+        printf("atof%f\n", atof(data));*/
+        //an[p] = atof(data);
+        an[p] = (float)((((int)data[0]) - 48) + (((int)data[2]) - 48) * 0.1 + (((int)data[3]) - 48) * 0.01 + (((int)data[4]) - 48) * 0.001);
+    }
+    else
+        an[p] = 0;
+
+}
+
+float average(float an[MemoryData])
+{
+    int t = 0;
+    float dis = 0;
+    for (int i = 0; i < MemoryData; i++)
+    {
+        if (an[i] != 0)
+        {
+            dis += an[i];
+            t++;
+        }
+    }
+    if (t != 0)
+        dis = dis / t;
+    return dis;
 }
 
 int main()
 {
     printf("Link Successed\n"); 
-    /*std::string str;
-    std::fstream f;
-    f.open("/dev/ttyS1");
-    while (f >> str)
-    {
-        std::cout << str;
-    }*/
     int fd;
     int nread;
-    char buff[50];
     /*以读写方式打开串口*/
-    char *ComAddress = "/dev/ttyS1";
-    fd = OpenDev(ComAddress);
-    /*设置串口属性*/
-    set_speed(fd, 115200);
-    if (set_Parity(fd, 8, 1, 'N') == false)
-    {
-        printf("Set Parity Error\n");
-        return 0;
-    }
-    /*进行读取操作*/
     while (1)
     {
-        while ((nread = read(fd, buff, 50)) > 0)
+        fd = open(ComAddress, O_RDONLY | O_NOCTTY);
+        if (fd < 0)//失败
         {
-            //printf("\nLen %d\n", nread);
-            buff[nread + 1] = '\0';
-            printf("\n%s", buff);
+            perror("Can't Open Serial Port\n");
+            sleep(1);
+            continue;
         }
+        std::cout << "Open Serial Port Successful\n";
+        break;
+    }
+    /*设置串口属性*/
+    setCom(fd);
+    /*进行读取操作*/
+    char buff[50];
+    float an0[MemoryData], an1[MemoryData], an2[MemoryData];
+    memset(an0, 0, MemoryData);
+    memset(an1, 0, MemoryData);
+    memset(an2, 0, MemoryData);
+    float dis0=0, dis1=0, dis2=0;
+    int p=0,t;
+
+    while (1)
+    {
+        if ((nread = read(fd, buff, 50)) > 0)
+        {
+            buff[nread] = '\0';
+            //printf("%s", buff);
+            GetData(buff, an0, "an0", p);
+            GetData(buff, an1, "an1", p);
+            GetData(buff, an2, "an2", p);
+            if (++p == 5)
+            {
+                p = 0;
+            }
+        }
+        dis0 = average(an0);
+        dis1 = average(an1);
+        dis2 = average(an2);
+        printf("0:%3.3f 1:%3.3f 2:%3.3f\n", dis0, dis1, dis2);
     }
     close(fd);
     return 0;
