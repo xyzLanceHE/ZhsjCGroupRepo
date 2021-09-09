1485002d960eadccca00360b7794b8ea50ebe349
LanceHE <xyzlancehe@gmail.com>
2021-09-09 11:15:11 +0800

    编写了地图消息,作为继承Message的案例

--- Project/Share/TinyROS/SharedMessageTypes.h
+++ Project/Share/TinyROS/SharedMessageTypes.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "TinyROSDef.h"
 #include "Messages.h"
 
 /// <summary>
@@ -29,15 +30,53 @@ namespace RoboTax
 #pragma endregion
 
 #pragma region 地图消息
-	// editor: 吴勇
+	// editor: 吴勇, 贺兰山
+
+	// Obsolete
 	// 地图结构体
-	struct Map
+	//struct Map
+	//{
+	//	int a[400];
+	//};
+
+	class MapMessage : public TinyROS::Message
 	{
-		int a[400];
+	public:
+		// 继承自Message
+		virtual TinyROS::TypeIDHash GetTypeID() override;
+		virtual std::string Serialize() override;
+		virtual void Deserialize(std::string& str) override;
+		virtual TinyROS::Message* NewDeserialize(std::string& str) override;
+		virtual ~MapMessage();
+	public:
+		// 创建一个空地图，长和宽等于0，数据指针为nullptr
+		MapMessage();
+		// 创建一个指定长宽的地图，所有元素都为0
+		MapMessage(int width, int height);
+		// 创建一个指定长宽的地图，并将数据复制过来。请保证数据长度匹配
+		MapMessage(int width, int height, unsigned char* pData);
+		// 拷贝构造
+		MapMessage(const MapMessage& other);
+		// 移动构造
+		MapMessage(MapMessage&& other);
+		// 获取地图上指定行、列的元素值，索引从0开始
+	    unsigned char At(int col, int row);
+		// 获取地图宽度（列数）
+		int GetWidth();
+		// 获取地图长度（行数）
+		int GetHeight();
+		// 将地图的数据复制到指定位置
+		void CopyTo(unsigned char* pBuffer);
+	private:
+		int Width;
+		int Height;
+		unsigned char* pData;
+		static TinyROS::TypeIDHash TypeHash;
 	};
 
+	// Obsolete
 	// 小车速度消息别名
-	using MapMessage = TinyROS::SimpleObjectMessage<Map>;
+	//using MapMessage = TinyROS::SimpleObjectMessage<Map>;
 #pragma endregion
 
 
